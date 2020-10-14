6747ad1dd8cc8de2fdeea0c40716dcf31c90ea2a
fahreddin <fahreddinglck97@gmail.com>
2020-10-14 18:31:54 +0300

    README.md added & hotfix

--- src/Containers/MainScreen/index.tsx
+++ src/Containers/MainScreen/index.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useMemo, useState} from 'react';
+import React, {useMemo, useState} from 'react';
 import {SafeAreaView, ScrollView} from 'react-native';
 import {Match} from '@Components/Match';
 import GestureRecognizer from 'react-native-swipe-gestures';
